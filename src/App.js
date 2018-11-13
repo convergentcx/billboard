6e27a5fa4c7e78d0449dfb6b0fb07cecbf2da7b8
Logan Saether <lsaether@protonmail.com>
2018-11-13 20:24:33 +0100

    rename repo

--- src/App.js
+++ src/App.js
@@ -8,16 +8,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="#"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Buy This Sign
           </a>
         </header>
       </div>
