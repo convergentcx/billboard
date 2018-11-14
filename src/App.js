c2fec46b46c3c37a2453d84dd1748343932f2aa3
Logan Saether <lsaether@protonmail.com>
2018-11-14 18:47:13 +0100

    add some nice material ui components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-/* eslint-disable no-undef */
 import React, { Component } from 'react';
-// import Web3 from 'web3';
+import {
+  Button,
+  Tooltip,
+ } from '@material-ui/core';
 
 import Chart from './components/Chart/Chart';
 
@@ -19,48 +21,93 @@ const mockCurveData = {
 class App extends Component {
   constructor(props) {
     super(props);
+    this.handleBuy = this.handleBuy.bind(this);
+    this.handleSell = this.handleSell.bind(this);
     this.state = {
       addr: 'hello_world',
-      bill: 'one'
+      billboard: {},
+      billboardAddress: 'unavailable',
+      curveData: mockCurveData,
     }
   }
 
   async componentDidMount() {
-    const { contracts, web3 } = this.props.drizzle;
-    console.log(contracts.Convergent_Billboard.address);
-    const me = (await web3.eth.getAccounts())[0]
+    const { contracts, store, web3 } = this.props.drizzle;
+    const { Convergent_Billboard: billboard } = contracts;
+    const me = (await web3.eth.getAccounts())[0];
+
+    // const state = store.getState();
+    // const exponentKey = billboard.methods.exponent.cacheCall();
+
+    // console.log(state.contracts.Convergent_Billboard)
+    const curveData = {
+      exponent: await billboard.methods.exponent().call(),
+      inverseSlope: await billboard.methods.inverseSlope().call(),
+      poolBalance: await billboard.methods.poolBalance().call(),
+      totalSupply: await billboard.methods.totalSupply().call(),
+    }
+    const currentPrice = (1 / curveData.inverseSlope) * (curveData.totalSupply) ** curveData.exponent;
+
+    Object.assign(curveData, { currentPrice });
+
     this.setState({
       addr: me,
-      bill: 'two'
+      billboard,
+      billboardAddress: billboard.address,
+      curveData
     })
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.curveData);
+  }
+
+  async handleBuy() {
+    this.state.billboard.methods.mint(String(10**18)).send({
+      from: this.state.addr,
+      value: await this.state.billboard.methods.priceToMint(String(10**18)).call(),
+    });
+  }
+
+  async handleSell() {
+    this.state.billboard.methods.burn(String(10**18)).send({
+      from: this.state.addr,
+    });
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <h1>Convergent Billboard</h1> */}
-          <h2>{this.state.addr}</h2>
-          <h2>{this.state.bill}</h2>
+        <Tooltip title={this.state.billboardAddress} placement="top" interactive>
+          <h1>Convergent Billboard</h1>
+        </Tooltip>
           <Chart
-            curveData={mockCurveData}
+            curveData={this.state.curveData}
             height="100%"
             width="100%"
             margin={{ top: 10, bottom: 10, left: 0, right: 50 }}
           />
-          <a
+          {/* <a
             className="App-link"
             href="#"
             target="_blank"
             rel="noopener noreferrer"
           >
             Buy This Sign
-          </a>
+          </a> */}
+          <div>
+            <Button color="primary" variant="outlined" onClick={this.handleBuy}>
+              Buy
+            </Button>
+            <Button color="secondary" variant="outlined" onClick={this.handleSell}>
+              Sell
+            </Button>
+          </div>
         </header>
       </div>
     );
   }
 }
 
-// export default App;
 export default withContext(App);
