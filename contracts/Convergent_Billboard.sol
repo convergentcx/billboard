1ce6119a785c63ef0cbe3437bdf8068184ed8c2e
Logan Saether <lsaether@protonmail.com>
2018-11-19 00:24:02 +0100

    test withdrawable billboard

--- contracts/Convergent_Billboard.sol
+++ contracts/Convergent_Billboard.sol
@@ -1,26 +1,40 @@
 pragma solidity ^0.4.24;
 
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "@convergent/arc/contracts/EthPolynomialCurvedToken.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract Convergent_Billboard is Ownable, EthPolynomialCurvedToken {
     using SafeMath for uint256;
 
-    uint256 public cashed; // Amount of tokens that have been "cashed out."
-    uint256  public requiredAmt = 1 * 10**18; // One token per banner change.
+    uint256 public cashed;                      // Amount of tokens that have been "cashed out."
+    uint256 public maxTokens = 500;             // Total amount of Billboard tokens to be sold.
+    uint256 public requiredAmt = 1 * 10**18;    // One token required per banner change.
 
     event Advertisement(bytes32 what, uint256 indexed when);
 
     constructor()
         EthPolynomialCurvedToken(
-            "Convergent Billboard",
-            "CNVRGNT_BILL",
+            "Convergent Billboard Token",
+            "CBT",
             18,
             1,
             1000
-        ) public
+        )
+        public
     {}
     
+    /// Overwrite 
+    function priceToMint(uint256 numTokens) public view returns(uint256) {
+        require(totalSupply().add(numTokens) < maxToken.mul(uint256(decimals())), "Must not have reached cap.");
+        return curveIntegral(totalSupply().add(numTokens)).sub(poolBalance);
+    }
+
+    /// Overwrite
+    function rewardForBurn(uint256 numTokens) public view returns(uint256) {
+        require(totalSupply() < maxToken.mul(uint256(decimals())), "Must not have reached cap.");
+        return poolBalance.sub(curveIntegral(totalSupply().sub(numTokens)));
+    }
+
     function purchaseAdvertisement(bytes32 _what) public payable {
         mint(requiredAmt);
         submit(_what);
@@ -46,4 +60,11 @@ contract Convergent_Billboard is Ownable, EthPolynomialCurvedToken {
 
         emit Advertisement(_what, block.timestamp);
     }
+
+    function withdraw() onlyOwner public {
+        require(totalSupply() >= maxToken.mul(uint256(decimals())), "Must have reached cap.");
+        owner().transfer(address(this).balance);
+    }
+
+    function () public { revert(); }
 }
