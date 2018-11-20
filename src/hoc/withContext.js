ad1225898ed23383e901bfabed1965b26df2361f
Logan Saether <lsaether@protonmail.com>
2018-11-20 12:52:37 +0100

    quick not connected page

--- src/hoc/withContext.js
+++ src/hoc/withContext.js
@@ -6,7 +6,21 @@ const withContext = WrappedComponent => props =>
     {drizzleContext => {
       const { drizzle, drizzleState, initialized } = drizzleContext;
       if (!initialized) {
-        return "Loading...";
+        return <main style={{ backgroundColor: 'white', display: 'flex', flexFlow: 'column wrap', height: '100vh' }}>
+          <div style={{ background: 'white', display: 'flex', justifyContent: 'center' }}>
+            <h1>⚠️</h1>
+          </div>
+          <div style={{ backgroundColor: 'white', paddingLeft: '25%', paddingRight: '25%', display: 'flex', justifyContent: 'center' }}>
+            <p>
+            <center>No Ethereum connection found!</center>
+            <br /><br />
+            <center>Please install <a href="https://metamask.io/" target="_blank" rel="noopener noreferrer">Metamask</a> or use the <a href="https://brave.com/download/" target="_blank" rel="noopener noreferrer">Brave</a> browser 
+            with Metamask installed.</center>
+            <br /><br />
+            <center>Make sure you're connected to Mainnet or the Rinkeby test network.</center>
+            </p>
+          </div>
+        </main>;
       }
 
       return (
